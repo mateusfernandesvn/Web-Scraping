6818851549caf57a554bf3c270668abbb67630bb
Mateus Fernandes <mateusveloso2608@gmail.com>
2025-02-02 23:49:08 -0300

    Script rodando automático.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import requests
+import schedule
+import time
 import csv
 from bs4 import BeautifulSoup
 from datetime import datetime
@@ -13,35 +15,42 @@ headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36"
 }
 
-try:
-    req = requests.get(url, headers=headers)
+def main():
+    try:
+        req = requests.get(url, headers=headers)
 
-    if req.status_code == 200:
-        print(f"✅ Conexão estabelecida! Código de status: {req.status_code}")
-        site = BeautifulSoup(req.text, 'html.parser')
+        if req.status_code == 200:
+            print(f"✅ Conexão estabelecida! Código de status: {req.status_code}")
+            site = BeautifulSoup(req.text, 'html.parser')
 
-        title = site.find("h1", class_="ui-pdp-title")
-        title = title.text.strip() if title else "Título não encontrado"
+            title = site.find("h1", class_="ui-pdp-title")
+            title = title.text.strip() if title else "Título não encontrado"
 
-        price = site.find("span", class_="andes-money-amount__fraction")
-        price = price.text.strip() if price else "Preço não encontrado"
-        
-        hours = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+            price = site.find("span", class_="andes-money-amount__fraction")
+            price = price.text.strip() if price else "Preço não encontrado"
+            
+            hours = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
 
-        nome_arquivo = "Dados.csv"
+            nome_arquivo = "Dados.csv"
 
-        with open(nome_arquivo, mode="a", newline="", encoding="utf-8") as arquivo_csv:
-            writer = csv.writer(arquivo_csv)
+            with open(nome_arquivo, mode="a", newline="", encoding="utf-8") as arquivo_csv:
+                writer = csv.writer(arquivo_csv)
 
-            if arquivo_csv.tell() == 0:
-                writer.writerow(["Data/Hora", "Título", "Preço"])
+                if arquivo_csv.tell() == 0:
+                    writer.writerow(["Data/Hora", "Título", "Preço"])
 
-            writer.writerow([hours, title, f"R${price}"])
+                writer.writerow([hours, title, f"R${price}"])
 
-        print(f"📂 Dados salvos em '{nome_arquivo}' com sucesso!")
+            print(f"📂 Dados salvos em '{nome_arquivo}' com sucesso!")
 
-    else:
-        print(f"❌ Erro ao acessar a página. Código de status: {req.status_code}")
+        else:
+            print(f"❌ Erro ao acessar a página. Código de status: {req.status_code}")
 
-except requests.exceptions.RequestException as e:
-    print(f"❌ Erro ao acessar a página: {e}")
+    except requests.exceptions.RequestException as e:
+        print(f"❌ Erro ao acessar a página: {e}")
+
+schedule.every().hours.do(main)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
